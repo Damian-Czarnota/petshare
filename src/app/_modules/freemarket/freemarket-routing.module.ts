f122b059f4dc641dc42f1a5011f36b93bf364377
Rafal Bochenski <rafal@therapify.eu>
2021-12-11 21:40:00 +0100

    Item checkout

--- src/app/_modules/freemarket/freemarket-routing.module.ts
+++ src/app/_modules/freemarket/freemarket-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
         children: []
     },
     {
-        path: 'checkout',
+        path: 'checkout/:slug',
         component: ListingCheckoutComponent,
         canActivate: [],
         children: []
--- src/app/_modules/freemarket/pages/listing-checkout/listing-checkout.component.ts
+++ src/app/_modules/freemarket/pages/listing-checkout/listing-checkout.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { ListingItem } from '@app/_core/models/listing-item';
+import { first } from 'rxjs/operators';
+import { LISTING_DATA } from 'src/data/listing.const';
 
 @Component({
   templateUrl: './listing-checkout.component.html',
   styleUrls: ['./listing-checkout.component.scss']
 })
-export class ListingCheckoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ListingCheckoutComponent {
+  item: ListingItem;
+  
+  constructor(private _activatedRoute: ActivatedRoute, private _title: Title) {
+    _activatedRoute.params
+      .pipe(first())
+      .subscribe(params => {
+        this.item = LISTING_DATA.find(p => p.slug === params.slug) || LISTING_DATA[0];
+        
+        _title.setTitle(`${this.item.title} - checkout | Pluto for You!`)
+      })
+    }
 
 }
